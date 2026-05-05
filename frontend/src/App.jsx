7cb6b8b091e0a02f0a6b66ffd7b3dbe478434280
Harshit <cr273916@gmail.com>
2026-05-05 13:51:22 +0530

    update the aws

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   // Backend API URL
-  const API_URL = 'http://127.0.0.1:8000/api/items/';
+  const API_URL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     fetchItems();
